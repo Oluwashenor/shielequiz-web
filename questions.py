27d62c72dc15410edfd2138356f0979fe749748a
Adeniyi <jasonzenith2017@gmail.com>
2026-08-29 12:22:17 +0100

    updated questions

--- questions.py
+++ questions.py
@@ -59,4 +59,204 @@ QUESTIONS = [
         },
         "correct_answer": "D",
     },
+    {
+        "question": "habiiyt31 the cat",
+        "options": {
+            "A": "German",
+            "B": "Nigeria",
+            "C": "Turkish",
+            "D": "Indonesia",
+        },
+        "correct_answer": "C",
+    },
+    {
+        "question": "Vatan",
+        "options": {
+            "A": "China",
+            "B": "Turkey",
+            "C": "Persian",
+            "D": "Korea",
+        },
+        "correct_answer": "B",
+    },
+    {
+        "question": "Unstoppable",
+        "options": {
+            "A": "India",
+            "B": "China",
+            "C": "Nigeria",
+            "D": "Indonesia",
+        },
+        "correct_answer": "C",
+    },
+    {
+        "question": "Pavel",
+        "options": {
+            "A": "Russia",
+            "B": "India",
+            "C": "Turkey",
+            "D": "Nigeria",
+        },
+        "correct_answer": "A",
+    },
+    {
+        "question": "Albert",
+        "options": {
+            "A": "Russia",
+            "B": "Spain",
+            "C": "France",
+            "D": "USA",
+        },
+        "correct_answer": "B",
+    },
+    {
+        "question": "Ivan",
+        "options": {
+            "A": "Ukraine",
+            "B": "Turkey",
+            "C": "Argentina",
+            "D": "Indonesia",
+        },
+        "correct_answer": "C",
+    },
+    {
+        "question": "habiiyt31 the cat",
+        "options": {
+            "A": "German",
+            "B": "Nigeria",
+            "C": "Turkish",
+            "D": "Indonesia",
+        },
+        "correct_answer": "C",
+    },
+    {
+        "question": "Pupu",
+        "options": {
+            "A": "Persian",
+            "B": "Vietnam",
+            "C": "China",
+            "D": "India",
+        },
+        "correct_answer": "B",
+    },
+    {
+        "question": "habiiyt31 the cat",
+        "options": {
+            "A": "German",
+            "B": "Nigeria",
+            "C": "Turkish",
+            "D": "Indonesia",
+        },
+        "correct_answer": "C",
+    },
+    {
+        "question": "MD rifat",
+        "options": {
+            "A": "Bangladeshi",
+            "B": "Nigeria",
+            "C": "Ukraine",
+            "D": "China",
+        },
+        "correct_answer": "A",
+    },
+    {
+        "question": "Edgars",
+        "options": {
+            "A": "Persian",
+            "B": "Latvia",
+            "C": "Argentina",
+            "D": "Ukraine",
+        },
+        "correct_answer": "B",
+    },
+    {
+        "question": "Ana Maria",
+        "options": {
+            "A": "Brazil",
+            "B": "Spain",
+            "C": "Turkey",
+            "D": "Columbia",
+        },
+        "correct_answer": "D",
+    },
+    {
+        "question": "Callmedoja",
+        "options": {
+            "A": "India",
+            "B": "Nigeria",
+            "C": "Argentina",
+            "D": "Russia",
+        },
+        "correct_answer": "B",
+    },
+    {
+        "question": "Dantist",
+        "options": {
+            "A": "Spain",
+            "B": "Indonesia",
+            "C": "Ukraine",
+            "D": "Korea",
+        },
+        "correct_answer": "C",
+    },
+    {
+        "question": "Mewt",
+        "options": {
+            "A": "China",
+            "B": "latam",
+            "C": "Ukraine",
+            "D": "India",
+        },
+        "correct_answer": "D",
+    },
+    {
+        "question": "Callmedoja",
+        "options": {
+            "A": "India",
+            "B": "Nigeria",
+            "C": "Argentina",
+            "D": "Russia",
+        },
+        "correct_answer": "B",
+    },
+    {
+        "question": "Pailosen",
+        "options": {
+            "A": "Ukraine",
+            "B": "China",
+            "C": "Korea",
+            "D": "Vietnam",
+        },
+        "correct_answer": "B",
+    },
+    {
+        "question": "JayK",
+        "options": {
+            "A": "Korea",
+            "B": "latam",
+            "C": "Spain",
+            "D": "Russia",
+        },
+        "correct_answer": "A",
+    },
+    {
+        "question": "TopVN",
+        "options": {
+            "A": "Ukraine",
+            "B": "Vietnam",
+            "C": "Argentina",
+            "D": "Turkey",
+        },
+        "correct_answer": "B",
+    },
+    {
+        "question": "Ander",
+        "options": {
+            "A": "Indonesia",
+            "B": "China",
+            "C": "Korea",
+            "D": "Nigeria",
+        },
+        "correct_answer": "A",
+    },
 ]
